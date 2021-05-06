bc660cf4993dce725e918a07e47b845f3af32d21
Cody Bloemhard <cblmhrd@live.nl>
2021-05-07 00:38:26 +0200

    refactor vertex, add samplebank

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # termdaw
 A daw, in a terminal.
+Termdaw is a (or should become a) terminal, graph based programmable pipeline digital audio workstation that is friendly to automatization and AI/Algorithmic composition.
+
+## MVP Goals
+- Terminal playback controls (play, pause, stop, set time, dash in time)
+- Graph rendering structure
+- Vertices, each with gain and panning:
+  * Output vertex
+  * Sample midi vertex (emit sample on note)
+  * Lv2 midi instrument vertex
+  * Sum vertex
+  * Normalize vertex
+  * Lv2 fx vertex
+- Lua
+  * To configure the project(sample rate, etc)
+  * Load resources (samples and plugins)
+  * Construct graph
+- Play back audio and render audio
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::collections::{ HashMap };
 
+use rubato::{ Resampler, SincFixedIn, InterpolationType, InterpolationParameters, WindowFunction };
 use mlua::prelude::*;
 
-fn main(){
+fn main() -> Result<(), String>{
+    let mut sample_bank = SampleBank::new(96000);
+    sample_bank.add("snare".to_owned(), "/home/cody/doc/samples/drumnbass/snare-1/snare-1-v-9.wav")?;
     lua_test();
+    Ok(())
 }
 
 pub struct Graph{
-    vertices: Vec<Box<dyn Node>>,
+    vertices: Vec<Box<dyn Vertex>>,
     edges: Vec<Vec<usize>>,
     names: HashMap<String, usize>,
     ran_status: Vec<bool>,
@@ -25,7 +29,7 @@ impl Graph{
         }
     }
 
-    pub fn add(&mut self, node: Box<dyn Node>, name: String){
+    pub fn add(&mut self, node: Box<dyn Vertex>, name: String){
         self.vertices.push(node);
         self.edges.push(Vec::new());
         let n = self.vertices.len() - 1;
@@ -49,11 +53,107 @@ impl Graph{
     }
 }
 
-pub type NodeRes<'a> = (usize, &'a[f32], &'a[f32]);
+pub trait Vertex{
+    fn read_buffer(&self) -> &Sample;
+    fn generate(&mut self, len: usize, res: Vec<&Sample>);
+}
 
-pub trait Node{
-    fn read_buffer(&self) -> NodeRes;
-    fn generate(&mut self, len: usize, res: Vec<NodeRes>);
+pub struct SampleBank{
+    sample_rate: usize,
+    samples: Vec<Sample>,
+    names: HashMap<String, usize>,
+}
+
+impl SampleBank{
+    pub fn new(sample_rate: usize) -> Self{
+        Self{
+            sample_rate,
+            samples: Vec::new(),
+            names: HashMap::new(),
+        }
+    }
+
+    pub fn add(&mut self, name: String, file: &str) -> Result<(), String>{
+        if self.names.get(&name).is_some() {
+            return Err(format!("TermDaw: SampleBank: there is already an sample with name \"{}\" present.", name));
+        }
+        let mut reader = if let Ok(reader) = hound::WavReader::open(file){
+            reader
+        } else {
+            return Err(format!("TermDaw: SampleBank: could not open file {}.", file));
+        };
+        let specs = reader.spec();
+        if specs.channels != 2 {
+            return Err(format!("TermDaw: SampleBank: only stereo samples are supported yet, found {} channels.", specs.channels));
+        }
+        let sr = specs.sample_rate as usize;
+        let bd = specs.bits_per_sample;
+        let mut l = Vec::new();
+        let mut r = Vec::new();
+        let mut c = 0;
+        if specs.sample_format == hound::SampleFormat::Float{
+            for s in reader.samples::<f32>(){
+                if s.is_err() { continue; }
+                let s = s.unwrap();
+                if c == 0 {
+                    l.push(s);
+                    c = 1;
+                } else {
+                    r.push(s);
+                    c = 0;
+                }
+            }
+        } else {
+            let max = ((1 << bd) / 2 - 1) as f32;
+            for s in reader.samples::<i32>(){
+                if s.is_err() { continue; }
+                let s = s.unwrap() as f32 / max;
+                if c == 0 {
+                    l.push(s);
+                    c = 1;
+                } else {
+                    r.push(s);
+                    c = 0;
+                }
+            }
+        }
+        if sr != self.sample_rate{ // need to resample
+            // no idea what is means but comes from the example lol
+            let params = InterpolationParameters {
+                sinc_len: 256,
+                f_cutoff: 0.95,
+                interpolation: InterpolationType::Nearest,
+                oversampling_factor: 160,
+                window: WindowFunction::BlackmanHarris2,
+            };
+            let mut resampler = SincFixedIn::<f32>::new(
+                self.sample_rate as f64 / sr as f64,
+                params, l.len(), 2
+            );
+            let waves_in = vec![l, r];
+            let mut waves_out = resampler.process(&waves_in).unwrap();
+            l = std::mem::replace(&mut waves_out[0], Vec::new());
+            r = std::mem::replace(&mut waves_out[1], Vec::new());
+        }
+        match Sample::from(l, r){
+            Ok(sample) => {
+                self.samples.push(sample);
+                self.names.insert(name, self.samples.len() - 1);
+                Ok(())
+            },
+            Err(e) => {
+                Err(e)
+            }
+        }
+    }
+
+    pub fn get_sample(&self, name: &str) -> Option<&Sample>{
+        if let Some(index) = self.names.get(name){
+            Some(&self.samples[*index])
+        } else {
+            None
+        }
+    }
 }
 
 pub struct Sample{
@@ -62,6 +162,23 @@ pub struct Sample{
 }
 
 impl Sample{
+    pub fn new(bl: usize) -> Self{
+        Self{
+            l: Vec::with_capacity(bl),
+            r: Vec::with_capacity(bl),
+        }
+    }
+
+    pub fn from(l: Vec<f32>, r: Vec<f32>) -> Result<Self, String>{
+        if l.len() != r.len() {
+            return Err(format!("TermDaw: Sample::from: l and r do not have the same length: {} and {}.", l.len(), r.len()));
+        }
+        if l.is_empty(){
+            return Err("TermDaw: Sample::from: l and r have length 0.".to_owned());
+        }
+        Ok(Self{ l, r })
+    }
+
     pub fn len(&self) -> usize{
         self.l.len()
     }
@@ -69,57 +186,57 @@ impl Sample{
     pub fn is_empty(&self) -> bool{
         self.l.is_empty()
     }
+
+    pub fn clear(&mut self){
+        self.l.clear();
+        self.r.clear();
+    }
 }
 
-pub struct SumNode{
-    buf_l: Vec<f32>,
-    buf_r: Vec<f32>,
+pub struct SumVertex{
+    buf: Sample,
 }
 
-impl SumNode{
+impl SumVertex{
     pub fn new(bl: usize) -> Self{
         Self{
-            buf_l: Vec::with_capacity(bl),
-            buf_r: Vec::with_capacity(bl),
+            buf: Sample::new(bl),
         }
     }
 }
 
-impl Node for SumNode{
-    fn read_buffer(&self) -> NodeRes{
-        (self.buf_l.len(), &self.buf_l, &self.buf_r)
+impl Vertex for SumVertex{
+    fn read_buffer(&self) -> &Sample{
+        &self.buf
     }
 
-    fn generate(&mut self, len: usize, res: Vec<NodeRes>){
-        self.buf_l.clear();
-        self.buf_r.clear();
-        let len = self.buf_l.len().min(len);
-        for (rlen, rl, rr) in res{
-            let l = rlen.min(len);
+    fn generate(&mut self, len: usize, res: Vec<&Sample>){
+        self.buf.clear();
+        let len = self.buf.len().min(len);
+        for r in res{
+            let l = r.len().min(len);
             for i in 0..l{
-                self.buf_l[i] += rl[i];
-                self.buf_r[i] += rr[i];
+                self.buf.l[i] += r.l[i];
+                self.buf.r[i] += r.r[i];
             }
         }
     }
 }
 
-pub struct SampleLoopNode{
-    sample: Sample,
+pub struct SampleLoopVertex<'a>{
+    sample: &'a Sample,
     playing: bool,
     t: usize,
-    buf_l: Vec<f32>,
-    buf_r: Vec<f32>,
+    buf: Sample,
 }
 
-impl SampleLoopNode{
-    pub fn new(sample: Sample, bl: usize) -> Self{
+impl<'a> SampleLoopVertex<'a>{
+    pub fn new(sample: &'a Sample, bl: usize) -> Self{
         Self{
             sample,
             playing: false,
             t: 0,
-            buf_l: Vec::with_capacity(bl),
-            buf_r: Vec::with_capacity(bl),
+            buf: Sample::new(bl),
         }
     }
 
@@ -132,22 +249,21 @@ impl SampleLoopNode{
     }
 }
 
-impl Node for SampleLoopNode{
-    fn read_buffer(&self) -> NodeRes{
-        (self.buf_l.len(), &self.buf_l, &self.buf_r)
+impl Vertex for SampleLoopVertex<'_>{
+    fn read_buffer(&self) -> &Sample{
+        &self.buf
     }
 
-    fn generate(&mut self, len: usize, _: Vec<NodeRes>){
+    fn generate(&mut self, len: usize, _: Vec<&Sample>){
         if self.playing{
             let l = self.sample.len();
-            for i in 0..self.buf_l.len().min(len){
-                self.buf_l[i] = self.sample.l[(self.t + i) % l];
-                self.buf_r[i] = self.sample.r[(self.t + i) % l];
+            for i in 0..self.buf.len().min(len){
+                self.buf.l[i] = self.sample.l[(self.t + i) % l];
+                self.buf.r[i] = self.sample.r[(self.t + i) % l];
             }
             self.t += l;
         } else {
-            self.buf_l.clear();
-            self.buf_r.clear();
+            self.buf.clear();
         }
     }
 }
@@ -175,21 +291,3 @@ fn lua_test() -> LuaResult<()> {
     Ok(())
 }
 
-
-// IceVec, a frozen size vector
-// struct IceVec<T>(Vec<T>,usize);
-//
-// impl<T> IceVec<T>{
-//     pub fn new(len: usize) -> Self{
-//         Self(Vec::<T>::with_capacity(len),len)
-//     }
-//
-//     pub fn len(&self) -> Self{
-//         self.0.len()
-//     }
-//
-//     pub fn push(&mut self, v: T) -> bool{
-//         if self.len()
-//     }
-// }
-//
